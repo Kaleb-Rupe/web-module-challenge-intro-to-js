6264616975b1157d76423c36606347c072bdb464
Kaleb-Rupe <57547999+Kaleb-Rupe@users.noreply.github.com>
2022-09-30 01:06:37 -0400

    Completed Challenge but left Stretch blank as I could not complete it with what I know at this point

--- index.js
+++ index.js
@@ -243,11 +243,12 @@ Using the feet function below do the following:
 3. Return number of feet
 */
 
-function feet(/*add your code here*/){
-  /*add your code here*/
+function feet(cm){
+  let convertedFeet = cm / 30.48;
+  return convertedFeet;
 }
 
-
+console.log(feet);
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
@@ -263,10 +264,13 @@ Using the annoyingSong function below do the following:
 4. Each time the annoyingSong is run from this loop, it should console.log the string that was returned. 
 */
 
-function annoyingSong(/*add your code here*/){
-      /*add your code here*/
+for (let i = 99; i > 1; i--) {
+  console.log(annoyingSong(i))
 }
 
+function annoyingSong(num) {
+    return(`${num} bottles of soda on the wall, ${num} bottles of soda, take one down pass it around ${num -1} bottles of soda on the wall`);
+}
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
@@ -283,10 +287,19 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(/*Your Code here */){
-/*Your Code here */
+function grade(score){
+  if (score >= 90){
+    return `you got an A`;
+  }else if (score >= 80 && score <= 89){
+    return `you got a B`;
+  }else if (score >= 70 && score <= 79){
+    return `you got a C`;
+  }else if (score >= 60 && score <= 69){
+    return `you got a D`;
+  }else if (score <= 59){
+    return `you got an F`;
+  }
 }
-
 
 
 /*💪💪💪💪💪💪💪💪💪💪 Stretch 💪💪💪💪💪💪💪💪💪💪*/
